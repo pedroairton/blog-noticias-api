33ad3ad197bbd811b79aac2b74a5d38f7988721b
Pedro Airton <111513226+pedroairton@users.noreply.github.com>
2026-01-10 14:18:28 -0300

    função para listar admins e autores

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin;
 
 class AdminController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Admin::where('role', 'author')
+            ->select(
+                'name',
+                'email',
+                'avatar',
+                'bio',
+                'website',
+                'social_facebook',
+                'social_twitter',
+                'social_instagram',
+                'social_linkedin'
+            )
+            ->paginate(20);
+        $admins = Admin::where('role', 'superadmin')
+            ->select(
+                'name',
+                'email',
+                'avatar',
+                'bio',
+                'website',
+                'social_facebook',
+                'social_twitter',
+                'social_instagram',
+                'social_linkedin'
+            )
+            ->paginate(20);
+        return response()->json(['authors' => $authors, 'admins' => $admins], 200);
+    }
 }
